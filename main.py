cdd53d811c798ae4e4119a8553c1f21a25dbe9dc
mavapa369 <mavapa369@yandex.ru>
2025-11-13 16:00:57 +0500

    added feature

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import uvicorn
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+  return {"message": "Hello World!"}
+
+operators = ["+", "-", "/", "*"]
 # сложение, вычитание, деление, умножение
 # addition, subtraction, division, multiplication
 @app.get("/addition")
@@ -21,6 +26,18 @@ async def div(a: int, b:int):
 async def mult(a: int, b:int):
   return {"result": a * b}
 
+@app.get("/custom_operation")
+async def custom(a:int, op: str, b: int):
+  if op == operators[0]:
+    return {"result": f"{a} + {b} = {a + b}"}
+  if op == operators[1]:
+    return {"result": f"{a} - {b} = {a - b}"}
+  if op == operators[2]:
+    return {"result": f"{a} / {b} = {int(a / b)}"}
+  if op == operators[3]:
+    return {"result": f"{a} * {b} = {a * b}"}
+
+# uvicorn main:app --reload main -название файла, app - переменна, где запускаем fastapi
 if __name__ == "__main__":
   uvicorn.run(
       "main:app",
@@ -28,4 +45,3 @@ if __name__ == "__main__":
       port=8000,
       reload=True,
   )
-
